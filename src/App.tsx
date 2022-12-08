05748dae9799d6c69f9f96819b1980a389fd9e41
Giovanny L. Silva <didiosilva395@gmail.com>
2022-12-07 21:48:47 -0400

    Adicionado leyout padrao

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,16 @@
-import { useState } from 'react';
-import { ThemeProvider } from 'styled-components';
-import { Button } from './components/Button';
-import { GlobalStyle } from './styles/global';
-import { defaultTheme } from './styles/themes/default';
+import { ThemeProvider } from 'styled-components'
+import { BrowserRouter } from 'react-router-dom'
+import { Router } from './Router'
+import { GlobalStyle } from './styles/global'
+import { defaultTheme } from './styles/themes/default'
 
 export function App() {
-  const [count, setCount] = useState(0)
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Button variant="primary"></Button>
-      <Button variant="secondary"></Button>
-      <Button variant="success"></Button>
-      <Button variant="danger"></Button>
-      <Button ></Button>
-      <GlobalStyle/>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+      <GlobalStyle />
     </ThemeProvider>
   )
 }
