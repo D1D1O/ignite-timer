e986eeea37ba35635e67e960c5df832309ccc03b
Giovanny L. Silva <didiosilva395@gmail.com>
2022-12-10 20:22:13 -0400

    Aprimorando inputs

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -19,10 +19,24 @@ export function Home() {
             type="text"
             id="task"
             placeholder="Dê um nome para o seu projeto"
+            list="task-suggestions"
           />
+          <datalist id="task-suggestions">
+            <option value="Projeto 1"></option>
+            <option value="Projeto 2"></option>
+            <option value="Projeto 3"></option>
+            <option value="Projeto 4"></option>
+          </datalist>
 
           <label htmlFor="minutesAmount">durante</label>
-          <MinutesAmountInput type="text" id="minutesAmount" placeholder="00" />
+          <MinutesAmountInput
+            type="number"
+            id="minutesAmount"
+            placeholder="00"
+            step={5}
+            min={5}
+            max={60}
+          />
           <span>minutos.</span>
         </FormContainer>
 
